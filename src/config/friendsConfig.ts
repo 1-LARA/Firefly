84ae13ab5dc855a238a6e7d8a5ab01191b81d899
annie <annie878948553@sjtu.edu.cn>
2026-08-11 15:38:38 +0800

    feat(friends): add friend link for elecannonic

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -2,7 +2,7 @@ import type { FriendLink, FriendsPageConfig } from "../types/friendsConfig";
 
 // 可以在src/content/spec/friends.md中编写友链页面下方的自定义内容
 
-// 友链页面配置
+// 页面配置
 export const friendsPageConfig: FriendsPageConfig = {
 	// 页面标题，如果留空则使用 i18n 中的翻译
 	title: "",
@@ -20,36 +20,18 @@ export const friendsPageConfig: FriendsPageConfig = {
 	randomizeSort: false,
 };
 
-// 友链配置
+// 配置
 export const friendsConfig: FriendLink[] = [
-	// {
-	// 	title: "夏夜流萤",
-	// 	imgurl:
-	// 		"https://weavatar.com/avatar/d252655d40d6874417a720bad0a6c5f77f8f6a1fd2f882f8f338402dc37e4190?s=640",
-	// 	desc: "飞萤之火自无梦的长夜亮起，绽放在终竟的明天。",
-	// 	siteurl: "https://blog.cuteleaf.cn",
-	// 	tags: ["Blog"],
-	// 	weight: 10, // 权重，数字越大排序越靠前
-	// 	enabled: true, // 是否启用
-	// },
-	// {
-	// 	title: "Firefly Docs",
-	// 	imgurl: "https://docs-firefly.cuteleaf.cn/logo.png",
-	// 	desc: "Firefly主题模板文档",
-	// 	siteurl: "https://docs-firefly.cuteleaf.cn",
-	// 	tags: ["Docs"],
-	// 	weight: 9,
-	// 	enabled: true,
-	// },
-	// {
-	// 	title: "Astro",
-	// 	imgurl: "https://avatars.githubusercontent.com/u/44914786?v=4&s=640",
-	// 	desc: "The web framework for content-driven websites. ⭐️ Star to support our work!",
-	// 	siteurl: "https://github.com/withastro/astro",
-	// 	tags: ["Framework"],
-	// 	weight: 8,
-	// 	enabled: true,
-	// },
+	{
+		title: "Elecannonic",
+		imgurl: "https://images.elecannonic.com/global/logo.png",
+		desc: "Elecannonic",
+		siteurl: "https://elecannonic.com",
+		tags: ["Blog"],
+		weight: 10, // 权重，数字越大排序越靠前
+		enabled: true, // 是否启用
+	},
+
 ];
 
 // 获取启用的友链并进行排序
